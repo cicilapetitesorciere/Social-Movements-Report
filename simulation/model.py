37169fb02a92b4ce8cb90c825988017aabe749a9
Ciaran Neely <cgtneely@uwaterloo.ca>
2023-04-02 21:40:31 -0400

    Finalized model

--- simulation/model.py
+++ simulation/model.py
@@ -1,53 +1,74 @@
+import sys
+import matplotlib.pyplot as plt
+
 from numpy import linspace
 from numpy import exp
 from scipy.integrate import odeint
-import matplotlib.pyplot as plt
-from typing import List, Final
+from typing import Final
 
 SUSCEPTIBLE_ENUM: Final = 0
 EXPOSED_ENUM: Final = 1
 INFECTED_ENUM: Final = 2
-RECOVERED_ENUM: Final = 3
 
-def model(y, t, N, k1, k2, k3, k4, k5, k6, k7, k8)
+def model(y, t, scale, k1, k2, k3, k4, k5, k6, k7, k8):
     
     susceptible =y[SUSCEPTIBLE_ENUM]
     exposed = y[EXPOSED_ENUM]
     infected = y[INFECTED_ENUM]
-    recovered = y[RECOVERED_ENUM]
 
-    offline = N - susceptible - exposed - infected
-    assert(offline >= 0)
+    offline = scale - susceptible - exposed - infected
+    
     trending = k8*(1-exp(-infected))
 
-    dy: List[float] = [0,0,0,0]
+    dy = [0,0,0]
 
-    dy[INFECTED_ENUM] = k6*offline + k2*infected + k5*exposed - k3*susceptible*infected - k7*susceptible - k4*trending*susceptible
+    dy[SUSCEPTIBLE_ENUM] = k6*offline + k2*infected + k5*exposed - k3*susceptible*infected - k7*susceptible - k4*trending*susceptible
     dy[EXPOSED_ENUM] = k3*infected*susceptible + k4*trending*susceptible - k5*exposed - k1*exposed*(1-infected/susceptible)
-    dy[SUSCEPTIBLE_ENUM] = k1*exposed*(1-infected/susceptible) - k2*infected
-    dy[RECOVERED_ENUM] = k7*susceptible
+    dy[INFECTED_ENUM] = k1*exposed*(1-infected/susceptible) - k2*infected
 
     return dy
 
-def simulate(t_lim_lower = 0, t_lim_upper = 10, initial_infected = 1, initial_exposed = 0, initial_susceptible = 1, initial_recovered = 0, n = 2, k1 = 1, k2 = 1, k3 = 1, k4 = 1, k5 = 1, k6 = 1, k7 = 1, k8 = 1)   
-    t_axis: List[float] = linspace(start=t_lim_lower, stop=t_lim_upper, num=100)
+def simulate_specific(  k1, k2, k3, k4, k5, k6, k7, k8,
+                        t_lim_lower = 0, 
+                        t_lim_upper = 52, 
+                        initial_infected = 0.5, 
+                        initial_exposed = 0, 
+                        initial_susceptible = 0.5, 
+                        n = 1
+                        ):
+    
+    t_axis = linspace(start=t_lim_lower, stop=t_lim_upper, num=100)
 
-    initial_y = [0,0,0,0]
+    initial_y = [0,0,0]
 
     initial_y[INFECTED_ENUM] = initial_infected
     initial_y[EXPOSED_ENUM] = initial_exposed
     initial_y[SUSCEPTIBLE_ENUM] = initial_susceptible
-    initial_y[RECOVERED_ENUM] = initial_recovered
 
-    simulation: List[List[float]] = odeint(
-        func=model,
-        y0=initial_y,
-        t=t_axis,
-        args=(n, k1, k2, k3, k4, k5, k6, k7, k8)
+    simulation = odeint(
+        func = model,
+        y0 = initial_y,
+        t = t_axis,
+        args = (n, k1, k2, k3, k4, k5, k6, k7, k8)
     )
 
-    plt.plot(t_axis, simulation[:,INFECTED_ENUM], label="Engaged in the social movement")
-    plt.plot(t_axis, simulation[:,EXPOSED_ENUM], label="Aware of the movement")
-    plt.plot(t_axis, simulation[:,SUSCEPTIBLE_ENUM], label="Active users")
-    plt.legend()
-    plt.show()
+    plt.plot(t_axis, simulation[:,SUSCEPTIBLE_ENUM], color="tab:green", linewidth=0.5)
+    plt.plot(t_axis, simulation[:,EXPOSED_ENUM], color="tab:orange", linewidth=0.5)
+    plt.plot(t_axis, simulation[:,INFECTED_ENUM], color="tab:blue", linewidth=0.5)
+    
+
+def simulate_broad(k1, k2, k3, k4, k5, k6, k7, k8,nweeks=52):
+    for iinf in [0.005, 0.01, 0.015]:
+        for isus in linspace(0.1,0.9):
+            simulate_specific(k1, k2, k3, k4, k5, k6, k7, k8,initial_infected=iinf, initial_susceptible=isus, t_lim_upper=nweeks)
+
+
+def legend():
+    plt.legend(["Unfamiliar with the movement", "Aware of the movment, but not actively involved", "Actively involved in the movement"])
+
+SAVE_MODE = True
+def draw(filename=False):
+    if (SAVE_MODE and type(filename).__name__ == 'str'):
+        plt.savefig("plots/" + filename)
+    else:
+        plt.show()
